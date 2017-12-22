fa8c21f55720bd03e0c2cfe902e795c157266a2a
John Salamon <john.salamon@sahmri000435.local>
2017-12-22 17:11:46 +1030

    ass

--- src/core/input.rs
+++ src/core/input.rs
@@ -16,6 +16,10 @@ impl<E> KeyBinder<E> {
     pub fn bind_key(&mut self, key: KeyboardInput, e: E) {
         self.bindings.insert(key.scancode, e); 
     }
+
+    pub fn return_binding(&mut self, key: KeyboardInput) -> Option<&E> {
+        self.bindings.get(&key.scancode)
+    }
 }
 
 pub enum DefaultBindings {
